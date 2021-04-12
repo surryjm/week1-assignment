ae2b16e1b2beacc2abd2b2a916bf04d720744374
Surry <50641921+surryjm@users.noreply.github.com>
2021-04-11 22:57:18 -0700

    finished lesson 1

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,16 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content, tag = 'div') {
+    super(content, tag);
+    this.content = content;
+    this.tag = tag;
+  }
+}
+const yodaQuote = new DivElement('Do or do not, there is no try');
+console.log(yodaQuote.render());
 
 // Export class here
+export default DivElement;
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,16 @@
 // Define class here
 
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
+const emersonQuote = new HTMLElement('p', 'Do not go where the path may lead, go instead where there is no path and leave a trail');
+console.log(emersonQuote.render());
 // Export class here
-export default {};
+export default HTMLElement;
